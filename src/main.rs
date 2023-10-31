774e697cf9ed7c4ce9982efaac6944d66fc02779
Khuu Viet Sang <sangkhuudev@gmail.com>
2023-10-31 16:49:47 +0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use lambda_http::{
+    http::header::CONTENT_TYPE, run, service_fn, Body, 
+    Request, Response, Error,
+};
+
+async fn function_handler(
+    event: Request,
+) -> Result<Response<Body>, Error> {
+    dbg!(event);
+    let html = "<html><body><h1>hello!</h1></body></html>";
+    let resp = Response::builder()
+        .status(200)
+        .header(CONTENT_TYPE, "text/html")
+        .body(Body::Text(html.to_string()))?;
+
+    Ok(resp)
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(service_fn(function_handler)).await
+}
